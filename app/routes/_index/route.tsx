7db28cd12c4373aad03157e572108433a9c170e7
Renato Cardoso <re2005@gmail.com>
2025-09-12 17:03:51 +0200

    chore: update domain

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -23,9 +23,7 @@ export default function App() {
     <div className={styles.index}>
       <div className={styles.content}>
         <h1 className={styles.heading}>A short heading about [your app]</h1>
-        <p className={styles.text}>
-          A tagline about [your app] that describes your value proposition.
-        </p>
+        <p className={styles.text}>Welcome to Ordery</p>
         {showForm && (
           <Form className={styles.form} method="post" action="/auth/login">
             <label className={styles.label}>
